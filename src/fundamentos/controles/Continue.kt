4a9827f26a643becf6e7f91ae4cd890c90f1bdbc
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-02-03 19:46:35 -0300

    add comments in continue

--- src/fundamentos/controles/Continue.kt
+++ src/fundamentos/controles/Continue.kt
@@ -1,9 +1,10 @@
 package fundamentos.controles
 
+// Evitar o uso
 fun main() {
     for(i in 1..10) {
         if(i==5)
-            continue // Vai para o próximo laço da repetição
+            continue // Vai para o próximo laço da repetição ( Pula )
 
         println("Atual: $i")
     }
